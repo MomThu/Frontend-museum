f86790d063ee863d82dc1dd62f8a62d18ff43148
Mom Thu <thumomm10@gmail.com>
2022-01-08 16:22:37 +0700

    last change

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://192.168.1.103:5000/';
-//export const baseUrl = 'http://10.0.2.2:5000/';
+//export const baseUrl = 'http://192.168.1.103:5000/';
+export const baseUrl = 'http://10.0.3.2:5000/';
